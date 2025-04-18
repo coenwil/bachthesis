1d3ed2ab7c23fa2c76d2446177c678943ddb54a6
coenw27 <coenwillemsen33@gmail.com>
2025-04-18 21:52:30 +0200

    Defining parameters

--- apriori/apriori.R
+++ apriori/apriori.R
@@ -1,2 +1,7 @@
 # function to determine sample size a priori using bayes factors
 # as described in Fu (2020)
+
+SSDlog <- function(n_min = 10, n_max = 100, 
+                   bf_thresh = 5, eta = 0.8, t = 10000) {
+  
+}
