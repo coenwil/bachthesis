e5907e51e8ff3e9e35decda6888daefd3aeb1fa8
coenw27 <coenwillemsen33@gmail.com>
2025-04-30 16:10:39 +0200

    helpers to compute fit and complexity

---
+++ helpers/compute_complexity.R
@@ -0,0 +1,21 @@
+# helper function to compute complexity
+# the area of the prior distribution that is in agreement with the hypothesis
+
+compute_complexity <- function(hypothesis = c("superiority", "non-inferiority", "equivalence"),
+                               delta = 0, var, b) {
+  
+  hypothesis <- match.arg(hypothesis)
+  
+  if (hypothesis == "superiority") {
+    return(pnorm(0, mean = 0, sd = (1/b) * sqrt(var), lower.tail = FALSE))
+  }
+  
+  if (hypothesis == "non-inferiority") {
+    return(pnorm(delta, mean = 0, sd = (1/b) * sqrt(var), lower.tail = FALSE))
+  }
+  
+  if (hypothesis == "equivalence") {
+    return(pnorm(delta, mean = 0, sd = (1/b) * sqrt(var), lower.tail = TRUE)) -
+      pnorm(-delta, mean = 0, sd = (1/b) * sqrt(var), lower.tail = TRUE)
+  }
+}
---
+++ helpers/compute_fit.R
@@ -0,0 +1,22 @@
+# helper function to compute fit
+# the area of the posterior distribution that is in agreement with the hypothesis
+
+compute_fit <- function(hypothesis = c("superiority", "non-inferiority", "equivalence"),
+                        delta = 0, coef, var) {
+  
+  hypothesis <- match.arg(hypothesis)
+  
+  if (hypothesis == "superiority") {
+    return(pnorm(0, mean = coef, sd = sqrt(var), lower.tail = FALSE))
+  }
+  
+  if (hypothesis == "non-inferiority") {
+    return(pnorm(delta, mean = coef, sd = sqrt(var), lower.tail = FALSE))
+  }
+  
+  if (hypothesis == "equivalence") {
+    return(pnorm(delta, mean = coef, sd = sqrt(var)) -
+             pnorm(-delta, mean = coef, sd = sqrt(var)))
+  }
+  
+}
