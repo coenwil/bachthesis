e78a415902c9f3764dbf2387a70d6e1fc9df072b
coenw27 <coenwillemsen33@gmail.com>
2025-06-19 01:20:20 +0200

    fixed data generation under complement hypothesis and renamed null -> complement

--- apriori/apriori.R
+++ apriori/apriori.R
@@ -26,8 +26,12 @@ SSDlog <- function(n_min = 10, n_max = 100,
   # generate y under null hypothesis (beta_1 = 0)
   y_null <- if (hypothesis %in% c("superiority", "non-inferiority")) {
     rbinom(pop_size, size = 1, prob = plogis(intercept + -beta_1*x))
+  # the beta for the null (=complement) hypothesis gets randomly selected from the range [0.3, 10]
+  # this is to reflect the fact that the complement of the equivalence trial can be anything
+  # as long as it's not in -delta, delta. 10 seems like a reasonable upper limit.
   } else if (hypothesis == "equivalence") {
-    rbinom(pop_size, size = 1, prob = plogis(intercept + (2*delta*x)))
+    beta_null <- runif(1, min = delta, max = 10)
+    rbinom(pop_size, size = 1, prob = plogis(intercept + (beta_null*x)))
   }
   pop_data <- data.table(x, y_alt, y_null)
   
@@ -84,9 +88,7 @@ SSDlog <- function(n_min = 10, n_max = 100,
       
       # computing complexity, fit
       comp_h1 <- compute_complexity(hypothesis, delta, var = var_h1, b)
-      
       fit_h1 <- compute_fit(hypothesis, delta, coef = coef_h1, var = var_h1)
-      
       # get the bayes factor
       # store all of them in a vector to get the mean later
       bf_h1 <- bayes_factor(fit_h1, comp_h1)
@@ -113,7 +115,7 @@ SSDlog <- function(n_min = 10, n_max = 100,
     
     # compute probability of bayes factor being above eta
     p_h1 <- mean(bf_h1_vec > bf_thresh)
-    p_h0 <- mean((1 / bf_h0_vec) > bf_thresh)
+    p_h0 <- mean(bf_h0_vec > bf_thresh)
     
     cat("n_mid:", n_mid, "p_h1:", p_h1, "p_h0", p_h0, "\n")
     
