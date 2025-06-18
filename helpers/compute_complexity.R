c1c4e257ce79355de1d0d753fd79500fe4fc0cac
coenw27 <coenwillemsen33@gmail.com>
2025-06-19 00:46:01 +0200

    fixed complexity under equivalence test (does not need to be 0.5)

--- helpers/compute_complexity.R
+++ helpers/compute_complexity.R
@@ -7,15 +7,16 @@ compute_complexity <- function(hypothesis = c("superiority", "non-inferiority", 
   hypothesis <- match.arg(hypothesis)
   
   if (hypothesis == "superiority") {
-    return(pnorm(0, mean = 0, sd = (1/b) * sqrt(var), lower.tail = FALSE))
+    return(pnorm(0, mean = 0, sd = b * sqrt(var), lower.tail = FALSE))
   }
   
   if (hypothesis == "non-inferiority") {
-    return(pnorm(delta, mean = delta, sd = (1/b) * sqrt(var), lower.tail = FALSE))
+    return(pnorm(delta, mean = delta, sd = sqrt(var / b), lower.tail = FALSE))
   }
   
   if (hypothesis == "equivalence") {
-    return(pnorm(delta, mean = 0, sd = (1/b) * sqrt(var), lower.tail = TRUE)) -
-      pnorm(-delta, mean = 0, sd = (1/b) * sqrt(var), lower.tail = TRUE)
+    return(pnorm(delta, mean = 0, sd = sqrt(var / b), lower.tail = TRUE) -
+      pnorm(-delta, mean = 0, sd = sqrt(var / b), lower.tail = TRUE))
   }
 }
+
